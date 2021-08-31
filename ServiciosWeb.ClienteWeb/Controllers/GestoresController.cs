10f883ad3c262a03ba28d8d8f5ec36858b05d2ce
mccode <mccode.master01@gmail.com>
2021-08-30 21:34:59 -0500

    Creacion de los metodos en el controlador con llamadas a las apis

--- ServiciosWeb.ClienteWeb/Controllers/GestoresController.cs
+++ ServiciosWeb.ClienteWeb/Controllers/GestoresController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Mvc;
 
@@ -28,6 +29,119 @@ namespace ServiciosWeb.ClienteWeb.Controllers
                 var listado = JsonConvert.DeserializeObject<List<Gestores>>(result);
                 //dgvGestores.DataSource = listado;
                 return View(listado);
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Gestores gestores)
+        {
+            //Invocar servicio rest api
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:49815/");
+
+            var request = clienteHttp.PostAsync("api/Gestores", gestores, new JsonMediaTypeFormatter()).Result;
+            /*clienteHttp.PostAsync();
+              clienteHttp.PutAsync();
+              clienteHttp.DeleteAsync();*/
+            if (request.IsSuccessStatusCode)
+            {
+                var result = request.Content.ReadAsStringAsync().Result;
+                var correcto = JsonConvert.DeserializeObject<bool>(result);
+                if (correcto)
+                {
+                    return RedirectToAction("Index");
+                }
+                //dgvGestores.DataSource = listado;
+                return View(gestores);
+            }
+            return View(gestores);
+        }
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            //Invocar servicio rest api
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:49815/");
+
+            var request = clienteHttp.GetAsync("api/Gestores/?id=" + id ).Result;
+            //var request = clienteHttp.GetAsync("api/Gestores/?id=" + id + "&nombre=" + param2).Result;
+            /*clienteHttp.PostAsync();
+              clienteHttp.PutAsync();
+              clienteHttp.DeleteAsync();*/
+            if (request.IsSuccessStatusCode)
+            {
+                var result = request.Content.ReadAsStringAsync().Result;
+                var gestor = JsonConvert.DeserializeObject<Gestores>(result);
+                return View(gestor);
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Actualizar(Gestores gestores)
+        {
+            //Invocar servicio rest api
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:49815/");
+
+            var request = clienteHttp.PutAsync("api/Gestores", gestores, new JsonMediaTypeFormatter()).Result;
+            /*clienteHttp.PostAsync();
+              clienteHttp.PutAsync();
+              clienteHttp.DeleteAsync();*/
+            if (request.IsSuccessStatusCode)
+            {
+                var result = request.Content.ReadAsStringAsync().Result;
+                var correcto = JsonConvert.DeserializeObject<bool>(result);
+                if (correcto)
+                {
+                    return RedirectToAction("Index");
+                }
+                //dgvGestores.DataSource = listado;
+                return View(gestores);
+            }
+            return View(gestores);
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int id)
+        {
+            //Invocar servicio rest api
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:49815/");
+
+            var request = clienteHttp.DeleteAsync("api/Gestores/?id=" + id).Result;
+            //var request = clienteHttp.GetAsync("api/Gestores/?id=" + id + "&nombre=" + param2).Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var result = request.Content.ReadAsStringAsync().Result;
+                var correcto = JsonConvert.DeserializeObject<bool>(result);
+                if (correcto)
+                {
+                    return RedirectToAction("Index");
+                }
+                //dgvGestores.DataSource = listado;
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Detalle(int id)
+        {
+            //Invocar servicio rest api
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:49815/");
+
+            var request = clienteHttp.GetAsync("api/Gestores/?id=" + id).Result;
+            //var request = clienteHttp.GetAsync("api/Gestores/?id=" + id + "&nombre=" + param2).Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var result = request.Content.ReadAsStringAsync().Result;
+                var gestor = JsonConvert.DeserializeObject<Gestores>(result);
+                return View(gestor);
             }
             return View();
         }
